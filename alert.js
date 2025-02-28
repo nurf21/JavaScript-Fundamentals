9f7d05d6dfff2b0f554bd48ee45f130ae0dc7f94
nurf21 <nurfauzangiri@gmail.com>
2025-02-28 12:01:57 +0700

    Added JavaScript code to declare variables, assign values, and display an alert box.

--- alert.js
+++ alert.js
@@ -1,1 +1,13 @@
 alert("I'm JavaScript!")
+
+// Declare variables
+let admin, name;
+
+// Assign value to name
+name = "John";
+
+// Copy value from name to admin
+admin = name;
+
+// Display admin value using alert
+alert(admin);  // Shows "John" in an alert box
