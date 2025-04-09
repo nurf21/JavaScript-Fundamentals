ecbfaa00e14b1bc73831332ffe9d3eb4f51af03f
nurf21 <nurfauzangiri@gmail.com>
2025-04-09 11:56:33 +0700

    Added new code examples to alert.js file

--- alert.js
+++ alert.js
@@ -21,3 +21,28 @@ let currentUserName = "John";
 
 // Usage example
 alert(`Welcome to ${ourPlanet}, ${currentUserName}!`);
+
+let answer = prompt('What is the "official" name of JavaScript?');
+if (answer === 'ECMAScript') {
+    alert('Right!');
+} else {
+    alert('You don\'t know? ECMAScript!');
+}
+
+let num = +prompt("Enter a number:");  // Convert input to a number
+
+if (num > 0) {
+    alert(1);
+} else if (num < 0) {x
+    alert(-1);
+} else {
+    alert(0);
+}
+
+let result = (a + b < 4) ? 'Below' : 'Over';
+
+let message =
+  login === 'Employee' ? 'Hello' :
+  login === 'Director' ? 'Greetings' :
+  login === ''         ? 'No login' : 
+  '';
