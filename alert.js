e88e6e3f4b1fc65d5bd202b69dcd713fe198718c
nurf21 <nurfauzangiri@gmail.com>
2025-04-14 14:21:50 +0700

    Added ask function and usage example to alert.js

--- alert.js
+++ alert.js
@@ -142,3 +142,14 @@ function checkAgeOr(age) {
 function min(a, b) {
   return a < b ? a : b;
 }
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
